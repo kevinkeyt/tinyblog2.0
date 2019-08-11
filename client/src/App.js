9f83f3e234440fc941a41714b6811299f25e018c
Kevin Keyt <kevinkeyt@gmail.com>
2019-08-11 14:44:46 -0400

    Initial component creation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,22 @@
 import React from 'react';
 
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import Nav from './layout/Nav';
+import Home from './Home';
+import SideColumn from './layout/SideColumn';
+
 function App() {
   return (
     <React.Fragment>
     <div className="container">
-      <header className="blog-header py-3">
-        <div className="row flex-nowrap justify-content-between align-items-center">
-          <div className="col-4 pt-1">
-            <a className="text-muted" href="#">Subscribe</a>
-          </div>
-          <div className="col-4 text-center">
-            <a className="blog-header-logo text-dark" href="#">Tiny Blog</a>
-          </div>
-          <div className="col-4 d-flex justify-content-end align-items-center">
-            <a className="text-muted" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
-            </a>
-            <a className="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
-          </div>
-        </div>
-      </header>
+      
+      <Header />
 
-      <div className="nav-scroller py-1 mb-2">
-        <nav className="nav d-flex justify-content-between">
-          <a className="p-2 text-muted" href="#">World</a>
-          <a className="p-2 text-muted" href="#">U.S.</a>
-          <a className="p-2 text-muted" href="#">Technology</a>
-          <a className="p-2 text-muted" href="#">Design</a>
-          <a className="p-2 text-muted" href="#">Culture</a>
-          <a className="p-2 text-muted" href="#">Business</a>
-          <a className="p-2 text-muted" href="#">Politics</a>
-          <a className="p-2 text-muted" href="#">Opinion</a>
-          <a className="p-2 text-muted" href="#">Science</a>
-          <a className="p-2 text-muted" href="#">Health</a>
-          <a className="p-2 text-muted" href="#">Style</a>
-          <a className="p-2 text-muted" href="#">Travel</a>
-        </nav>
-      </div>
+      <Nav />
 
-      <div className="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-        <div className="col-md-6 px-0">
-          <h1 className="display-4 font-italic">Title of a longer featured blog post</h1>
-          <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-          <p className="lead mb-0"><a href="#" className="text-white font-weight-bold">Continue reading...</a></p>
-        </div>
-      </div>
+      <Home />
 
-      <div className="row mb-2">
-        <div className="col-md-6">
-          <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-primary">World</strong>
-              <h3 className="mb-0">Featured post</h3>
-              <div className="mb-1 text-muted">Nov 12</div>
-              <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="stretched-link">Continue reading</a>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-success">Design</strong>
-              <h3 className="mb-0">Post title</h3>
-              <div className="mb-1 text-muted">Nov 11</div>
-              <p className="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="stretched-link">Continue reading</a>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
     
     <main role="main" className="container">
@@ -116,49 +57,13 @@ function App() {
             <a className="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
           </nav>
         </div>
-        <aside className="col-md-4 blog-sidebar">
-          <div className="p-4 mb-3 bg-light rounded">
-            <h4 className="font-italic">About</h4>
-            <p className="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-          </div>
-
-          <div className="p-4">
-            <h4 className="font-italic">Archives</h4>
-            <ol className="list-unstyled mb-0">
-              <li><a href="#">March 2014</a></li>
-              <li><a href="#">February 2014</a></li>
-              <li><a href="#">January 2014</a></li>
-              <li><a href="#">December 2013</a></li>
-              <li><a href="#">November 2013</a></li>
-              <li><a href="#">October 2013</a></li>
-              <li><a href="#">September 2013</a></li>
-              <li><a href="#">August 2013</a></li>
-              <li><a href="#">July 2013</a></li>
-              <li><a href="#">June 2013</a></li>
-              <li><a href="#">May 2013</a></li>
-              <li><a href="#">April 2013</a></li>
-            </ol>
-          </div>
-
-          <div className="p-4">
-            <h4 className="font-italic">Elsewhere</h4>
-            <ol className="list-unstyled">
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Facebook</a></li>
-            </ol>
-          </div>
-        </aside>
+        
+        <SideColumn />
       </div>
     </main>
     
 
-    <footer className="blog-footer">
-      <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
+      <Footer />
     </React.Fragment>
   );
 }
